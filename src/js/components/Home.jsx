584058b6d073ba447641b7d23b1cce6a09a58a32
Hugo Alexander <165641301+Galexhu@users.noreply.github.com>
2025-10-27 20:39:00 +0000

    cambios fix

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("");
+	const [todos, setTodos] = useState("");
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1>To-Do List</h1>
+			<form>
+				<input
+					type="text"
+					onChange={(event) => setInputValue(event.target.value)}
+					value={inputValue}
+					onKeyPress={(event) => {
+						console.log("key was pressed", event.key);
+						if (event.key == 23) setTodos(todos.concat(inputValue))
+					}}
+					placeholder="Cosas pendientes" />
+			</form>
+			<ul>
+				<li>
+					Sacar a las perras a pasear <i className="fa-solid fa-trash"></i>
+				</li>
+				<li>
+					Hacer la compra<i className="fa-solid fa-trash"></i>
+				</li>
+				<li>
+					Estudiar para el examen de React<i className="fa-solid fa-trash"></i>
+				</li>
+				<li>
+					Limpiar la casa<i className="fa-solid fa-trash"></i>
+				</li>
+				<li>
+					Preparar la cena<i className="fa-solid fa-trash"></i>
+				</li>
+			</ul>
+			<div>23 tareas pendientes</div>
 		</div>
 	);
 };
