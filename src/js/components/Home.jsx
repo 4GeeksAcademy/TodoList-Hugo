219915e11f6163dacb9b5e1b6f1d5486d6954908
Hugo Alexander <165641301+Galexhu@users.noreply.github.com>
2025-10-28 18:27:16 +0000

    ya funciona

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -7,39 +7,25 @@ import React, { useState } from "react";
 //create your first component
 const Home = () => {
 	const [inputValue, setInputValue] = useState("");
-	const [todos, setTodos] = useState("");
+	const [todos, setTodos] = useState([]);
+	function agregarTarea(){
+		setTodos([...todos,inputValue])
+		setInputValue("")
+	}
 	return (
 		<div className="container">
 			<h1>To-Do List</h1>
-			<form>
+			
 				<input
 					type="text"
-					onChange={(event) => setInputValue(event.target.value)}
 					value={inputValue}
-					onKeyPress={(event) => {
-						console.log("key was pressed", event.key);
-						if (event.key == 23) setTodos(todos.concat(inputValue))
-					}}
-					placeholder="Cosas pendientes" />
-			</form>
+					onChange={(event) => setInputValue(event.target.value)}
+					placeholder="Cosas pendientes"/>
+				<button onClick={agregarTarea}>Agregar tarea</button>	
 			<ul>
-				<li>
-					Sacar a las perras a pasear <i className="fa-solid fa-trash"></i>
-				</li>
-				<li>
-					Hacer la compra<i className="fa-solid fa-trash"></i>
-				</li>
-				<li>
-					Estudiar para el examen de React<i className="fa-solid fa-trash"></i>
-				</li>
-				<li>
-					Limpiar la casa<i className="fa-solid fa-trash"></i>
-				</li>
-				<li>
-					Preparar la cena<i className="fa-solid fa-trash"></i>
-				</li>
+				{todos.map(todo=> <li key={todo}>{todo}</li>)}
 			</ul>
-			<div>23 tareas pendientes</div>
+			<div>{todos.length} tareas pendientes</div>
 		</div>
 	);
 };
