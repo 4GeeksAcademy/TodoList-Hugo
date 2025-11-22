e86a340e2f1c8c0bdee59dcb8c471c08c1a0d388
Hugo Alexander <165641301+Galexhu@users.noreply.github.com>
2025-11-22 18:27:14 +0000

    fix final

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,36 +1,52 @@
 import React, { useState } from "react";
 
 
-//include images into your bundle
-//import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
 	const [inputValue, setInputValue] = useState("");
 	const [todos, setTodos] = useState([]);
+	
 	function agregarTarea(){
-		setTodos([...todos,inputValue])
+		if(!inputValue.trim() ) return;
+		setTodos([...todos,inputValue.trim()])
 		setInputValue("")
 	}
-	function quitarTarea(){
+	function quitarTarea(indexTarea){
+		setTodos(todos.filter((_, indexActual) => indexActual !== indexTarea));
 
 	}
 
 	return (
-		<div className="container">
-			<h1>To-Do List</h1>
-			
+		<div className="container text-center mt5">
+			<h1 className="display-3 text-primary fw-light">To-Do List</h1>
+			<div className="row justify-content-center">
+                <div className="col-10 col-md-6 d-flex mb-3"></div>
 				<input
 					type="text"
+					className="form-control form-control-lg shadow-sm"
 					value={inputValue}
 					onChange={(event) => setInputValue(event.target.value)}
-				
+					onKeyDown={(event) => event.key === "Enter" && agregarTarea()}
 					placeholder="Cosas pendientes"/>
-				<button onClick={agregarTarea}>Agregar tarea</button>	
-			<ul>
-				{todos.map(todo=> <li key={todos}>{todos}<i className="fa-solid fa-trash"></i> </li>)}
+
+				<button 
+				className="btn btn-primary btn-lg ms-2 shadow-sm"
+				onClick={agregarTarea}>Agregar tarea</button>
+			</div>
+				
+			<ul className="list-group shadow mx-auto">
+				{todos.map((tarea, indiceTarea) =>(
+					<li 
+					className="list-group-item d-flex justify-content-between align-items-center" 
+					key={indiceTarea}>{tarea}
+					<i 
+					className="fa-solid fa-trash ms-2 text-danger" 
+					onClick={() => quitarTarea(indiceTarea)} 
+					style={{color:"crimson"}}>
+						</i>
+						</li>
+				))}
 			</ul>
-			<div>{todos.length} tareas pendientes</div>
+			<p className="text-muted mt-3">{todos.length} tareas pendientes</p>
 		</div>
 	);
 };
